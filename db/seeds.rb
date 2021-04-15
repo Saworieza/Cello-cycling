7fc97a1633fa5123e366c9f94b7d042c45c6e59a
saworieza <saworieza@gmail.com>
2021-04-15 19:02:52 +0300

    Bike seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+bikes = Bike.create([{biketype: 'MTB', description: 'Mountain Bike'}, {biketype: 'Blackie', description: 'Black Mamba'}])
