6939fa743d4cf753202668675df4ec96e55e48f7
saworieza <saworieza@gmail.com>
2021-04-15 18:58:50 +0300

    fixed payment to rental relationship

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  resources :payments
   resources :rentals
   resources :bikes
-  resources :payments
   resources :cellos
   get 'home/index'
   root 'home#index'
--- db/migrate/20210415155759_create_payments.rb
+++ db/migrate/20210415155759_create_payments.rb
@@ -1,7 +1,7 @@
 class CreatePayments < ActiveRecord::Migration[5.2]
   def change
     create_table :payments do |t|
-      t.string :amount
+      t.decimal :amount
       t.references :cello, foreign_key: true
       t.references :rental, foreign_key: true
 
